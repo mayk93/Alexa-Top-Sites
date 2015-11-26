8444e2519414b145529015148b3f883c2e0e749e
Mihai Mandrescu <mihai.mandrescu@gmail.com>
2015-11-26 11:54:19 +0000

    Python3 compatible.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name  = 'alexa-top-sites',
 version     = '0.1',
 packages    = ['alexa'],
-url = 'http://github.com/davedash/Alexa-Top-Sites',
+url = 'https://github.com/mayk93/Alexa-Top-Sites.git',
 author = 'Dave Dash',
 author_email = 'dd+github@davedash.com',
 )
